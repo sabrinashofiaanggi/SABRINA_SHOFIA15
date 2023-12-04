a31cd171adf4e15506f40a9414382edfc8107cd8
sabrinashofiaanggi <sabrinashofiaanggi@gmail.com>
2023-12-04 17:39:52 +0700

    second commit

---
+++ sabrina_shofia15/__init__.py
@@ -0,0 +1,9 @@
+"""
+
+pyexample.
+An example python library.
+"""
+
+_version_ = '0.1.0'
+_author_ = 'sabrinashofiaanggi'
+_credits_ = 'Politeknik Negeri Semarang'
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SABRINA_SHOFIA15',
+    version='0.1.0',
+    author='sabrinashofiaanggi',
+    author_email='sabrinashofiaanggi@gmail.com',
+    description='Repository untuk mengerjakan jobsheet13',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
